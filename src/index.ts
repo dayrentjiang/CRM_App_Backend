50f0232bbaade3e1a532d379bc65790c98cf708e
dayrentjiang <dayrentjiang@gmail.com>
2026-01-09 16:46:32 +1100

    setting up Prisma ORM

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+import "dotenv/config";
 import express, { type Express, type Request, type Response } from "express";
 import ServerConfig from "./config/server.config";
+import { PrismaClient } from "./generated/prisma";
+
 import cors from "cors";
 
 const app: Express = express();
+const prisma = new PrismaClient();
 
 app.use(
   cors({
@@ -16,6 +20,14 @@ app.get("/ping", (req: Request, res: Response) => {
   res.json({ message: "hello from ping" });
 });
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
   console.log(`Server start in port ${ServerConfig.PORT} `);
+  const user = await prisma.user.create({
+    data: {
+      name: "Rich",
+      email: "rich@example.com",
+      password: "12345678",
+    },
+  });
+  console.log(user);
 });
